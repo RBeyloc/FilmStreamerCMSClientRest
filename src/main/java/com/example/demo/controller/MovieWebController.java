29e83ff6e282a69a78d470707a10da0c9146fa52
Ricard Beyloc Ros <rbeyloc@gmail.com>
2022-06-02 19:47:21 +0200

    Commit#3
    Testing New Movie Form

--- src/main/java/com/example/demo/controller/MovieWebController.java
+++ src/main/java/com/example/demo/controller/MovieWebController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.model.Movie;
 
 @Controller
 @RequestMapping("/movies")
@@ -29,6 +30,20 @@ public class MovieWebController {
     @RequestMapping("/newMovieForm")
     public String newMovieForm(Model containerToView) {
         return "newMovieForm";
+    }
+
+    @RequestMapping("/createMovie")
+    public String createMovie(@RequestParam String title, @RequestParam String releaseYear,
+                              @RequestParam String duration, @RequestParam String directing,
+                              @RequestParam String casting, @RequestParam String synopsis,
+                              @RequestParam String genre, @RequestParam String ageRating,
+                              @RequestParam String posterPath,  @RequestParam String videoPath,
+                              Model containerToView) {
+        Movie newMovie = new Movie(title, Integer.valueOf(releaseYear), Integer.valueOf(duration),
+                directing, casting, synopsis, genre, ageRating, posterPath, videoPath);
+        movieService.createMovie(newMovie);
+        containerToView.addAttribute("allMovies", movieService.getAllMovies().get());
+        return "showMovies";
     }
 
 }
