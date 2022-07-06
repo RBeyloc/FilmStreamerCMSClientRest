c50e15db9fe1eb056586810a312d3d89b3ff10b5
RBeyloc <102668219+RBeyloc@users.noreply.github.com>
2022-07-06 11:28:14 +0200

    Ammend getting rid of MongoDB tests and User Service and Repository based on this.

--- src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-import java.util.UUID;
-
-public interface UserRepository extends MongoRepository<User, String> {}
