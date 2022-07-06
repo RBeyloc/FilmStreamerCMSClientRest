c65bda7cac80d35899aff8edd3bdf4aec5b2c7fe
RBeyloc <102668219+RBeyloc@users.noreply.github.com>
2022-07-06 11:46:19 +0200

    Restore of MongoDB tests and User Service and Repository based on this.

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {}
